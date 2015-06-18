e297e27682ca64a9fc3d2c394dcf9abc361767db
scneptune <rmalcome@gmail.com>
2015-06-17 23:03:22 -0700

    clean up these Walker classes so they utilize the proper variables as params instead of the arbitrary one's made by their authors.

--- lib/cleanassnav.php
+++ lib/cleanassnav.php
@@ -1,38 +1,38 @@
 <?php
-class MV_Cleaner_Walker_Nav_Menu extends Walker {
+class MV_Cleaner_Walker_Nav_Menu extends \Walker {
     var $tree_type = array( 'post_type', 'taxonomy', 'custom' );
     var $db_fields = array( 'parent' => 'menu_item_parent', 'id' => 'db_id' );
-    function start_lvl(&$output, $depth) {
+    function start_lvl(&$output, $depth = 0, $args = []) {
         $indent = str_repeat("\t", $depth);
         $output .= "\n$indent<ul class=\"sub-menu\">\n";
     }
-    function end_lvl(&$output, $depth) {
+    function end_lvl(&$output, $depth = 0, $args = []) {
         $indent = str_repeat("\t", $depth);
         $output .= "$indent</ul>\n";
     }
-    function start_el(&$output, $item, $depth, $args) {
+    function start_el(&$output, $object, $depth = 0, $args = [], $current_object_id = 0) {
         global $wp_query;
         $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
         $class_names = $value = '';
-        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+        $classes = empty( $object->classes ) ? array() : (array) $object->classes;
         $classes = in_array( 'current-menu-item', $classes ) ? array( 'current-menu-item' ) : array();
-        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $object, $args ) );
         $class_names = strlen( trim( $class_names ) ) > 0 ? ' class="' . esc_attr( $class_names ) . '"' : '';
-        $id = apply_filters( 'nav_menu_item_id', '', $item, $args );
+        $id = apply_filters( 'nav_menu_item_id', '', $object, $args );
         $id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
         $output .= $indent . '<li' . $id . $value . $class_names .'>';
-        $attributes  = ! empty( $item->attr_title ) ? ' class="'  . esc_attr( $item->attr_title ) .'"' : '';
-        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
-        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+        $attributes  = ! empty( $object->attr_title ) ? ' class="'  . esc_attr( $object->attr_title ) .'"' : '';
+        $attributes .= ! empty( $object->target )     ? ' target="' . esc_attr( $object->target     ) .'"' : '';
+        $attributes .= ! empty( $object->xfn )        ? ' rel="'    . esc_attr( $object->xfn        ) .'"' : '';
+        $attributes .= ! empty( $object->url )        ? ' href="'   . esc_attr( $object->url        ) .'"' : '';
         $item_output = $args->before;
         $item_output .= '<a'. $attributes .'>';
-        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+        $item_output .= $args->link_before . apply_filters( 'the_title', $object->title, $object->ID ) . $args->link_after;
         $item_output .= '</a>';
         $item_output .= $args->after;
-        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $object, $depth, $args );
     }
-    function end_el(&$output, $item, $depth) {
+    function end_el(&$output, $object, $depth = 0, $args = []) {
         $output .= "</li>\n";
     }
 }
